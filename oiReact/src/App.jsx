76c065d34c30e62e92783d9932a7f82968253868
SuyaneNogueira <Suyane-Nogueira@estudante.sesisenai.org.br>
2024-08-23 17:16:13 -0300

    Criação do primeiro projeto com react bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <h1>Dinossauros</h1>
+    <h2>Carnivoros</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Facere mollitia itaque, saepe dolorum commodi vel voluptatum ab voluptate culpa,
+       expedita quaerat. Deserunt reiciendis nam doloribus eius magnam doloremque maxime dolor?</p>
+
+    </>
+  )
+}
+
+export default App
